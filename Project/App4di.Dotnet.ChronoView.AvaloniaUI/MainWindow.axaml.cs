da7f7bccca828db907f58cf426ee55a24031d658
Robert Oze <support@4dillusions.com>
2026-02-21 01:22:17 +0100

    add AvaloniaUI

---
+++ Project/App4di.Dotnet.ChronoView.AvaloniaUI/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace App4di.Dotnet.ChronoView.AvaloniaUI;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
