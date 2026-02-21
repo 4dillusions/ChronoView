2ed78af311b9b514b32fe8e3ba69e8826ed151b8
Robert Oze <support@4dillusions.com>
2026-02-21 13:50:06 +0100

    add (tab) pages

--- Project/App4di.Dotnet.ChronoView.AvaloniaUI/App.axaml.cs
+++ Project/App4di.Dotnet.ChronoView.AvaloniaUI/App.axaml.cs
@@ -1,6 +1,13 @@
+/*
+4di .NET ChronoView application
+Copyright (c) 2025 by 4D Illusions. All rights reserved.
+Released under the terms of the GNU General Public License version 3 or later.
+*/
+
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+//using Avalonia.Styling;
 
 namespace App4di.Dotnet.ChronoView.AvaloniaUI;
 
@@ -13,11 +20,13 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+        //RequestedThemeVariant = ThemeVariant.Light;
+        
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = new MainWindow();
         }
-
+        
         base.OnFrameworkInitializationCompleted();
     }
 }
